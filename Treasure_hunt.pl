9c903c75f8c932a26cda3f3f3e6fca3fc5a4c558
Serhii Vakulenko <sergeyvakulenko22@gmail.com>
2021-04-04 20:18:22 +0300

    create goTo() function

--- Treasure_hunt.pl
+++ Treasure_hunt.pl
@@ -34,7 +34,12 @@ instructions :-
 control_commands :-    
     write("Control commands:"),nl,
     write("  state - the state of the game."), nl,
-	write("  goto(X). - where X is a place to go to."), nl,
+    write("  help - shoq instrunctions again."), nl,
+	write("  quit - exit the game."),nl,
+	write("  goTo(X) - where X is a place to go to."), nl,
+%	write("  shoot(X). - where X is your shooting target."), nl,
+%	write("  take(X). - where X is a thing to take."), nl,
+%	write("  chase(X). - chasing ducks sends them to the pen."), nl,
 	nl.
 
 :- dynamic(loc/2).
@@ -42,7 +47,7 @@ control_commands :-
 
 % ------------ перевірка поточного стану -------
 
-loc(egg,cave).
+loc(gold,cave).
 loc(ducks,cave).
 loc(you,cave).
 loc(farmer,yard).
@@ -53,14 +58,14 @@ state :-
 	loc(you, L), 
     write(L), nl,
 	state_can_go(L),
-	look_here(L),
-	look_have(you).
+	state_see(L),
+	state_have(you).
 
-% ------------ перевірка куди я можу goto(x) в залежності від поточного мого місця -------
+% ------------ перевірка куди я можу goTo(x) в залежності від поточного мого місця -------
 
 state_can_go(L) :-
 	write("Can go to: "),
-	connect(L, CAN_GO_TO),
+	connection(L, CAN_GO_TO),
 	write(" "), 
     write(CAN_GO_TO), nl,
 	fail.
@@ -71,7 +76,65 @@ beside(yard, house).
 beside(house, pond).
 beside(yard, pond).
 
-connect(X,Y) :-
+connection(X,Y) :-
         beside(X,Y).
-connect(X,Y) :-
+connection(X,Y) :-
         beside(Y,X).
+
+% ------------ що ще є в поточному місці (що я можу бачити) -------
+
+state_see(L) :-
+	write("I see: "),
+	loc(THING, L),
+	THING \= you,
+	write(" "), write(THING), nl,
+	fail.
+state_see(_).
+
+% ------------ що має герой -------
+
+state_have(X) :-
+	write("Have: "),
+	loc(THING, X),
+	write(" "), write(THING), nl,
+	fail.
+state_have(_).
+
+% ------------ сontrol commands   -------
+
+do(state) :- !, state.
+do(goTo(X)) :- !, goTo(X).
+do(help) :- !, instructions.
+do(quit) :- !.
+do(X) :- write("unknown command"), write(X), nl, control_commands.
+
+% ------------ хід гри   -------
+
+go :- done.
+go :-
+	write(">> "),
+	read(X),
+	X \= quit,
+	do(X),
+%	demons,
+	!, go.
+go :- write("You have quit"), nl.
+
+% ------------ реалізація goTo(X)   -------
+
+goTo(X) :-
+	loc(you, L),
+	connection(L, X),
+	move(you, X),
+	write("You are in the "), write(X), nl.
+
+goTo(_) :-
+	write("You can't get there from here."), nl.
+
+% http://www.cse.unsw.edu.au/
+
+% assert - це мета-предикат, який додає свій єдиний аргумент, 
+% який може бути фактом або правилом, до бази даних Prolog.
+
+% dynamic - вказівник, що цей предикат динамічний і може змінюватись 
+% під час виконання програми 	
